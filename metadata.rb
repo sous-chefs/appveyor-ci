f214fe7bfcc838d3cb8651393f63b7026aef7868
Dan Webb <dan.webb@damacus.io>
2016-05-12 11:37:22 +0100

    Bump version for fixes

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email 'dan.webb@damacus.io'
 license 'Apache 2.0'
 description 'Installs AppVeyor Agent & Triggers Deployment'
 long_description 'Installs AppVeyor Agent & Triggers Deployment'
-version '0.1.0'
+version '0.1.1'
 
 depends 'windows'
