84e90757f663990c2526e8c832b86bdb42353466
Dan Webb <dan.webb@damacus.io>
2016-09-23 16:39:54 +0100

    Version bump to 0.1.3

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+# Appveyor-api Changelog
+
+## 0.1.3
+ - Uses HTTPS for download the agent
+ - Fixes kitchen testing in AppVeyor
--- metadata.rb
+++ metadata.rb
@@ -6,7 +6,7 @@ description 'Installs AppVeyor Agent & Triggers Deployment'
 long_description 'Installs AppVeyor Agent & Triggers Deployment'
 issues_url 'https://github.com/damacus/chef-appveyor-ci/issues'
 source_url 'https://github.com/damacus/chef-appveyor-ci'
-version '0.1.2'
+version '0.1.3'
 
 depends 'windows'
 
