5e8737d169fc041affb0228604119210787bf77c
Tim Smith <tsmith84@gmail.com>
2019-07-24 16:04:42 -0700

    Use chef style comment headers
    auto-corrected with cookstyle
    
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- resources/agent_install.rb
+++ resources/agent_install.rb
@@ -1,8 +1,8 @@
 
-# Cookbook Name:: appveyor-agent
+# Cookbook:: appveyor-agent
 # Resource:: agent
 #
-# Copyright (C) 2016 J Sainsburys PLC
+# Copyright:: (C) 2016 J Sainsburys PLC
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
